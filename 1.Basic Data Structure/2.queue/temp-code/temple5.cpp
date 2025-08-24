740456f6276505b089d4778b246fca431c7036d7
jianyanya <benbeny.tong@qq.com>
2025-08-24 20:38:24 +0800

    ds

--- 1.Basic Data Structure/2.queue/temp-code/temple5.cpp
+++ 1.Basic Data Structure/2.queue/temp-code/temple5.cpp
@@ -2,8 +2,32 @@
 //从不定问题变为限定子数组长度问题,就是在题目中加上返回m长度的子数组最大和
 //这个时候可以用定长滑窗和单调队列来做
 //说实话滑窗太简单了...
-
+//那如何用单调队列做呢,我们记住,无论怎么操作,我们都可以给单调队列定义队头为最值,队列的作用就是维护一个固定的窗口,并把最小值放在最左边以便取出
+//既然最小值在队列左边,那我枚举最大值不就完了吗,要得到范围内的值,使用前缀和
+//单调队列
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;cin>>n>>m;
+    vector<int> arr(n+1);
+    for(int i = 0;i<n;i++){
+        int m;cin>>m;
+        arr[i+1] = arr[i]+m;//预处理前缀和
+    }
+    deque<int> dq;
+    dq.push_back(0);//初始化;
+    int ans = 0;
+    for(int i = 1;i<=n;i++){
+        while(!dq.empty()&&dq.front()<i-m) dq.pop_front();//思考为什么这里不是<=,因为有个0在前面
+        ans = max(ans,arr[i]-arr[dq.front()]);//答案条件
+        while(!dq.empty()&&arr[i]<=arr[dq.back()]) dq.pop_back();//比我大的闪开
+        dq.push_back(i);
+    }
+    cout<<ans;
+    return 0;
+}
 //定长滑窗模板
+/*
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
@@ -28,3 +52,4 @@ int main(){
     cout<<right<<endl;
     return 0;
 }
+*/    
